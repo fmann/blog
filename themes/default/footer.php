7ff9fa1e6964c31277d863d4113665d381e89063
Floyd Mann <floyd@affinitybridge.com>
2017-01-03 09:58:24 -0800

    First commit.

---
+++ themes/default/footer.php
@@ -0,0 +1,11 @@
+		<div class="wrap">
+	            <footer id="bottom">
+	                <ul role="navigation">
+	                    <li><a href="<?php echo rss_url(); ?>">RSS</a></li>
+	                </ul>
+	            </footer>
+
+	        </div>
+        </div>
+    </body>
+</html>
